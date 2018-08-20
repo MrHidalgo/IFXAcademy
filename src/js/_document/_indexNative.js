afb9af1985beb724f979429f39d397bf427a1e20
MrHidalgo <Luckyhidalgo@gmail.com>
2018-08-20 13:32:44 +0300

    Initial commit

---
+++ src/js/_document/_indexNative.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * @description Document DOM ready.
+ */
+(function() {
+  /**
+   *
+   * @type {*|jQuery|HTMLElement}
+   * @private
+   */
+  const _document = document,
+    _window = window;
+
+
+  /**
+   * @description Init all method
+   */
+  const initNative = () => {
+    // ...
+  };
+  initNative();
+
+
+  /**
+   * =============================================
+   * CALLBACK
+   * =============================================
+   */
+})();
